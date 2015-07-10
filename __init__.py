369a5065ceb55daef439cae75113d12b7786a276
Christopher L.P. Wolfe <christopher.wolfe@stonybrook.edu>
2015-07-10 15:35:14 -0400

    Setting up my module.

--- __init__.py
+++ __init__.py
@@ -1,1 +1,13 @@
+"""
+timeseries module
+
+Summary
+-------
+Tools for time series analysis.
+
+Contains
+--------
+mtmSpectrum : A class implementing the multitaper method for spectral analysis.
+"""
+
 __author__ = 'cwolfe'
