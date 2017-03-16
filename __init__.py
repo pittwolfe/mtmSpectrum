4f5729b467a0be2746c54e78715f8948e5ef0c31
Christopher L.P. Wolfe <christopher.wolfe@stonybrook.edu>
2017-03-16 15:06:04 -0400

    Added utility functions to calculate the DPSSs

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 """
-timeseries module
+mtmSpectrum module
 
 Summary
 -------
@@ -8,6 +8,7 @@ Tools for time series analysis.
 Contains
 --------
 mtmSpectrum : A class implementing the multitaper method for spectral analysis.
+util : Some functions to calculates the DPSSs.
 """
 
 __author__ = 'cwolfe'
